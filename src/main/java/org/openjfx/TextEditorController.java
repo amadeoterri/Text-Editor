f43ee4d967b712c497f63995cb2c998b5b187e5a
amadeoterri <amadeoterri@gmail.com>
2020-11-16 16:57:56 +0100

    first commit

---
+++ src/main/java/org/openjfx/TextEditorController.java
@@ -0,0 +1,14 @@
+package org.openjfx;
+
+import javafx.event.ActionEvent;
+
+public class TextEditorController {
+
+    public void openFile(ActionEvent actionEvent) {
+
+
+    }
+
+    public void saveFile(ActionEvent actionEvent) {
+    }
+}
